bcf730900004eac7720712376f3c09d8d91eca51
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-07-08 08:52:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from './Home/Home'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+     <Routes>
+      <Route path="/" element={<Home />} /> 
+     </Routes>
+      
+    
+    </>
+  )
+}
+
+export default App
